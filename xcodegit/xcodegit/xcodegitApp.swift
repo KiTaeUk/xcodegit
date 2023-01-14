4ea4d507817e6d286eed9f5e75e255de40d70858
KiTaeUk <ktw1421@gmail.com>
2023-01-14 13:15:21 +0900

    first version(commit)

---
+++ xcodegit/xcodegit/xcodegitApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodegitApp.swift
+//  xcodegit
+//
+//  Created by 기태욱 on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct xcodegitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
