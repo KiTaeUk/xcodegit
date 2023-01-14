4ffd6ead86a2dee9df2a8e19cd828e8e2f10ec70
KiTaeUk <ktw1421@gmail.com>
2023-01-14 13:31:39 +0900

    branch change

---
+++ xcodegit/xcodegit/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  xcodegit
+//
+//  Created by 기태욱 on 2023/01/14.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
